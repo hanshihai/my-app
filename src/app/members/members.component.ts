f193a0ea877b660cbc7ad8e42cffeb17aac4d500
hans <shi-hai.han@dxc.com>
2019-05-17 18:28:43 +0800

    enable sync example

---
+++ src/app/members/members.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Member} from '../member';
+import { MEMBERS } from '../mock-members';
+import { MemberServiceService } from '../member-service.service';
+
+@Component({
+  selector: 'app-members',
+  templateUrl: './members.component.html',
+  styleUrls: ['./members.component.less']
+})
+export class MembersComponent implements OnInit {
+	
+  members: Member[];
+
+  selectedMember: Member;
+  
+  constructor(private memberService: MemberServiceService) { }
+
+  ngOnInit() {
+	  this.getMembers();
+  }
+
+  onSelect(member: Member): void {
+	  this.selectedMember = member;
+  }
+  
+  getMembers(): void {
+	  this.members = this.memberService.getMembers();
+  }
+}
---
+++ src/app/mock-members.ts
@@ -0,0 +1,9 @@
+import { Member } from './member';
+
+export const MEMBERS: Member[] = [
+	{id: 1, name: 'Mr. A'},
+	{id: 2, name: 'Mr. B'},
+	{id: 3, name: 'Mr. C'},
+	{id: 4, name: 'Mr. D'},
+	{id: 5, name: 'Mr. E'}
+];
